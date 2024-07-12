faf7c238ca16d69ffff2e7f67e296e2fe7f56155
itou-syunya-0826 <146901442+itou-syunya-0826@users.noreply.github.com>
2024-07-12 15:35:44 +0900

    joinの位置をかえた

--- PG3_7-2 std入門(thread)/main.cpp
+++ PG3_7-2 std入門(thread)/main.cpp
@@ -4,23 +4,22 @@
 int current_thread = 1;
 
 void print_thread(int thread_id) {
-    while (true) {
-        if (current_thread == thread_id) {
-            printf("thread%d\n", thread_id);
-            current_thread++;
-            break;
-        }
-    }
+	while (true) {
+		if (current_thread == thread_id) {
+			printf("thread%d\n", thread_id);
+			current_thread++;
+			break;
+		}
+	}
 }
 
 int main() {
-    std::thread t1(print_thread, 1);
-    std::thread t2(print_thread, 2);
-    std::thread t3(print_thread, 3);
+	std::thread t1(print_thread, 1);
+	t1.join();
+	std::thread t2(print_thread, 2);
+	t2.join();
+	std::thread t3(print_thread, 3);
+	t3.join();
 
-    t1.join();
-    t2.join();
-    t3.join();
-
-    return 0;
+	return 0;
 }
